971d0a802123f0216b91baa130ccc8a532321039
ivo922 <59027079+ivo922@users.noreply.github.com>
2022-03-22 08:18:35 +0200

    Add todo file

---
+++ src/todo.js
@@ -0,0 +1,18 @@
+/**
+ * TODO:
+ * ! Note header not showing on tables that don't have items with notes.
+ * ! Note remains active when switching tabs.
+ *
+ * * Weapons table
+ * * Trinkets table
+ * * Builds
+ * * Calculator
+ * * Favicon
+ * * Github pages deploy
+ *
+ * * Rewrite builds and item notes using markdown.
+ *
+ * ? Sort items
+ * ? Add leech to the calculator
+ * ? Add stats lost to DR to calculator
+ */
